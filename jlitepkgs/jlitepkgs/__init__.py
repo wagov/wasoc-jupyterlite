e43f9c601e0c94fb2dcbf766805eb491155042dc
Adon Metcalfe <adonm@fastmail.fm>
2022-10-15 14:53:53 +0800

    worked around pyodide odd imports

--- jlitepkgs/jlitepkgs/__init__.py
+++ jlitepkgs/jlitepkgs/__init__.py
@@ -2,15 +2,18 @@ import js
 from requests import request
 import pyodide_http
 import pandas
+
 pyodide_http.patch_all()
 
 def http_call(path: str, method: str = "GET", **kwargs):
-    return request(f"{js.origin}/{path}", method=method, **kwargs)
+    return request(url=f"{js.origin}/{path}", method=method, **kwargs)
 
-def kql2df(kql, workspace: str, timespan="P1D"):
+
+def kql2df(kql, workspace: str = None, timespan="P1D"):
     params = {"query": kql, "timespan": timespan}
     path = "/siemqueryutils/api/v1/globalQuery"
     if workspace:
         path = "/siemqueryutils/api/v1/simpleQuery"
         params["name"] = workspace
-    return pandas.DataFrame(http_call(path, params = params).json())
+    response = http_call(path, params=params)
+    return pandas.DataFrame(response.json())
