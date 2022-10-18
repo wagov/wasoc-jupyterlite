3ba02c4ee8514fa89d3d2d2e51c07ad4bf1dc01f
Adon Metcalfe <adonm@fastmail.fm>
2022-10-18 17:21:15 +0800

    tweaked paths to align with updated api endoints

--- jlitepkgs/jlitepkgs/__init__.py
+++ jlitepkgs/jlitepkgs/__init__.py
@@ -6,14 +6,16 @@ import pandas
 pyodide_http.patch_all()
 
 def http_call(path: str, method: str = "GET", **kwargs):
-    return request(url=f"{js.origin}/{path}", method=method, **kwargs)
+    # Assumes https://github.com/wagov/siem-query-utils/blob/main/siem_query_utils/proxy.py mounted at /proxy
+    return request(url=f"{js.origin}/proxy/{path}", method=method, **kwargs)
 
 
 def kql2df(kql, workspace: str = None, timespan="P1D"):
+    # Assumes https://github.com/wagov/siem-query-utils/blob/main/siem_query_utils/api.py mounted at /api/v1
     params = {"query": kql, "timespan": timespan}
-    path = "/siemqueryutils/api/v1/globalQuery"
+    path = "/api/v1/globalQuery"
     if workspace:
-        path = "/siemqueryutils/api/v1/simpleQuery"
+        path = "/api/v1/simpleQuery"
         params["name"] = workspace
     response = http_call(path, params=params)
     return pandas.DataFrame(response.json())
