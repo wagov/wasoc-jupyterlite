1bbc544671173a2f2f6b0ab6e93b4448899debb2
Adon Metcalfe <adonm@fastmail.fm>
2022-12-05 15:30:59 +0800

    updated for latest version of siem query utils endpoints

--- jlitepkgs/jlitepkgs/__init__.py
+++ jlitepkgs/jlitepkgs/__init__.py
@@ -6,13 +6,5 @@ import pandas
 pyodide_http.patch_all()
 
 def http_call(path: str, method: str = "GET", **kwargs):
-    # Assumes https://github.com/wagov/siem-query-utils/blob/main/siem_query_utils/proxy.py mounted at /proxy
-    return request(url=f"{js.origin}/proxy/{path}", method=method, **kwargs)
-
-
-def kql2df(kql, timespan="P1D"):
-    # Assumes https://github.com/wagov/siem-query-utils/blob/main/siem_query_utils/api.py mounted at /api/v1
-    params = {"timespan": timespan}
-    path = "/api/v2/query"
-    response = request(url=f"{js.origin}{path}", method="POST", params=params, data=kql)
-    return pandas.DataFrame(response.json())
+    # Assumes https://github.com/wagov/siem-query-utils/blob/main/siem_query_utils/proxy.py mounted at /
+    return request(url=f"{js.origin}/{path}", method=method, **kwargs)
